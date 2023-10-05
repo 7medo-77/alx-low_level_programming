ad91e1ee2e093fee36bab55673f324b10a9ecce6
root <root@7medo>
2023-10-05 22:33:53 +0300

    Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,11 +13,13 @@ int main(void)
 {
 	int i, j;
 
-	char *s = "and that piece of art is useful \" - Dora Korpar, 2015-10-19"
+	char *s = "and that piece of art is useful \" - Dora Korpar, 2015-10-19";
+
 	for (i = 0; s != '\0'; i++)
 	{
 		j++;
 	}
+
 	write(2, s, j);
 	return (1);
 }
