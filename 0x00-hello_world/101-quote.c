3283a6e7d4efa95626b949f1600b175c6b1168e3
root <root@7medo>
2023-10-05 22:12:10 +0300

    Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main - Entry point for the program 
+ *main- Entry point for the program
  *
- * Description: A C program which write to the standard error
+ *Description: A C program which write to the standard error
  *
- * Return: always 0 (Success)
+ *Return: always 0 (Success)
  */
 
-int main (void)
+int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful \" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19");
 	return (1);
-}	
+}
