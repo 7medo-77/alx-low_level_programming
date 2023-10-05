1c55f10ee3293d01a97b2d338522d447669c5295
root <root@7medo>
2023-10-05 22:27:22 +0300

    Advanced

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  *main- Entry point for the program
@@ -11,6 +11,12 @@
 
 int main(void)
 {
-	write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19");
+	int i, j;
+	char *s = "and that piece of art is useful \" - Dora Korpar, 2015-10-19"
+	for (i = 0; s != '\0'; i++)
+	{
+		j++;
+	}
+	write(2, s, j);
 	return (1);
 }
