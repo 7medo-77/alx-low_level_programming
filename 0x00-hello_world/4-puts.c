ffe93ce074f49a701a76645ee1cb458766d7e10c
root <root@7medo>
2023-10-05 20:45:45 +0300

    mand + 1-adv

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,8 +7,8 @@
  * Return: Always 0 (success)
  */
 
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle.\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
