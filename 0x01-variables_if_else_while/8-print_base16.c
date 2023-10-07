25ec707fec4e6d07e419f0e5f8947795239ae3bd
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-07 07:56:18 +0300

    3-print alphabets

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- program entry point
+ *
+ * Description: A C program printing all hex characters
+ *
+ * return: 0 (success)
+ */
+int main(void)
+{
+	char d = '0';
+
+	while (d <= 'f')
+		if ((d >= '0' && d <= '9') || (d >= 'a' && d <= 'f'))
+			putchar(d++);
+	putchar('\n');
+	return (0);
+}
