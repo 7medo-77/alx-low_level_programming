a03c72d35eea8e94b8662e9a4bc5a8ff58deb790
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-12 08:25:37 +0300

    nums

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - a function that prints a dotted line with n length
+ *
+ * @n: number which specifies the length of line
+ *
+ * Return: Always 0 (success)
+ */
+void print_line(int n)
+{
+	while (n--)
+		_putchar('_');
+
+	_putchar('_');
+	_putchar('\n');
+}
+
