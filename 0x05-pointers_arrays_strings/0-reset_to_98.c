bad2d96eb81dd66a39360986c5bdc9789a4deda1
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-16 22:24:49 +0300

    first file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * function_name - A function name
+ *
+ * @c: Description for parameter name
+ *
+ * Return: Always 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	int a;
+
+	a = 98;
+	*n = a;
+}
