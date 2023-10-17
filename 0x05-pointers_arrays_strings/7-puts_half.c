b4bdc8326abddec5bdeca29db7dd0f763d876a8d
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-17 19:53:34 +0300

    7-puts_half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,9 +13,9 @@ void puts_half(char *str)
 	int len, j;
 
 	len = 0;
-	while (*str)
+	while (*str++)
 		len++;
-	j = (len - 1) / 2;
+	j = (len + 1) / 2;
 
 	for (; str[j] != '\0'; j++)
 	{
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * function_name - A function that
+ *
+ * @c: A paramter entered into the function
+ *
+ * Return: always 0 (success)
+ */
+void print_array(int *a, int n)
+{
+	
+}
