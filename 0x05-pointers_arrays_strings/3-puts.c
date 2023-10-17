5e300d2e55da939dbdc1db4fca53a93fc6ea4d10
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-17 11:44:11 +0300

    3-puts mod + 4-print_rev

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,14 +3,12 @@
  * _puts - A function that prints a string, followed by a new line
  *
  * @str: The string passed to the argument
- *
- * Return: always 0 (Sucess)
  */
 void _puts(char *str)
 {
 	int i;
 
 	for (i = 0; *str != '0'; i++)
-		_putchar(str[i]);
+		_putchar((str[i]) + '0');
 	_putchar('\n');
 }
