e866028af83b26da48db2340fd20972aae246191
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-18 13:36:57 +0300

    README + 0-strcat_init

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - A function that counts the number of elements in an array
+ *
+ * @c: A paramter array which is entered into the function
+ *
+ * Return: Integer which represents the number of elemnts in an array(success)
+ *
+ */
+int _strlen(char *c)
+{
+	int a;
+
+	a = 0;
+	while (c[a] != '\0')
+		a++;
+	return (a);
+}
+
+/**
+ * function_name - A function that
+ *
+ * @dest: A paramter
+ * @src:
+ *
+ * Return: Always 0 (success)
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *concat;
+	int len_src, len_dest, i, j;
+
+	len_src = _strlen(src);
+	len_dest = _strlen(dest);
+	return (concat);
+}
+
+
