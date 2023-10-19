de40a2919bd23630938b852f5e7a26a5d3d1cbdb
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-19 15:32:27 +0300

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,18 +7,25 @@
  * @dest: A parameter which src is added to
  * @n: Number of bytes from string src
  *
- * Return: A pionter to the concatenated array
+ * Return: Always 0 (success)
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i, j, k;
 
 	i = 0;
 	while (dest[i] != '\0')
 		i++;
 
-	for (j = 0; j <= n; j++)
+	k = 0;
+	while (src[k] != '\0')
+		k++;
+
+	if (n > k)
+		n = k;
+
+	for (j = 0; j <= n && src[j] != '\0'; j++)
 	{
 		if (j == n)
 			dest[i] = '\0';
@@ -28,4 +35,3 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	return (dest);
 }
-
