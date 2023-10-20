5ffb7d4df48d5de5c2b0d9b6ed31c6201d70592f
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-20 03:25:53 +0300

    6-cap_string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,4 +5,24 @@
  *
  * @s: String which would be capitalized
  */
-char *cap_string(char *string)
+char *leet(char *string)
+{
+	char letters[] = "AaEeOoTtLl";
+	char numbers[] = "4433007711";
+	int i, j;
+
+	i = 0;
+	while (string[i] != '\0')
+	{
+		j = 0;
+		while (letters[j] != '\0')
+		{
+			if (string[i] == letters[j])
+			{
+				string[i] = numbers[j];
+			}
+			j++;
+		}
+		i++;
+	}
+}
