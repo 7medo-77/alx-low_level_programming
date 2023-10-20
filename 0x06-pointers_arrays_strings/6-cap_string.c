2d84ff1d2290d2a6f5c2f5576e155b92b13b5146
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-20 03:14:22 +0300

    6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * string_toupper - A function that capitalizes lowercase letters in a string
+ * cap_string - A function that capitalizes lowercase letters in a string
  *
- * @s: String which would be capitalized
+ * @string: String which would be capitalized
  */
 char *cap_string(char *string)
 {
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * string_toupper - A function that capitalizes lowercase letters in a string
+ *
+ * @s: String which would be capitalized
+ */
+char *cap_string(char *string)
