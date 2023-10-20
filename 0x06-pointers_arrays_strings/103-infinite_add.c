e896f83e00408669946363277a316ddf110a8d84
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-20 14:13:23 +0300

    Infinite add task

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rev_string - A function which reverses an array
+ * rev_string - reverse array
  * @n: integer params
  * Return: 0
  */
@@ -27,7 +27,7 @@ void rev_string(char *n)
 }
 
 /**
- * infinite_add - add 2 numbers together with no limit
+ * infinite_add - add 2 numbers together
  * @n1: text representation of 1st number to add
  * @n2: text representation of 2nd number to add
  * @r: pointer to buffer
@@ -37,8 +37,8 @@ void rev_string(char *n)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int over_flow = 0, i = 0, j = 0, nums = 0;
-	int val1 = 0, val2 = 0, temporary = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
 		i++;
@@ -48,7 +48,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	j--;
 	if (j >= size_r || i >= size_r)
 		return (0);
-	while (j >= 0 || i >= 0 || over_flow == 1)
+	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
 			val1 = 0;
@@ -58,21 +58,21 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			val2 = 0;
 		else
 			val2 = *(n2 + j) - '0';
-		temporary = val1 + val2 + over_flow;
-		if (temporary >= 10)
-			over_flow = 1;
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
 		else
-			over_flow = 0;
-		if (nums >= (size_r - 1))
+			overflow = 0;
+		if (digits >= (size_r - 1))
 			return (0);
-		*(r + nums) = (temporary % 10) + '0';
-		nums++;
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
 		j--;
 		i--;
 	}
-	if (nums == size_r)
+	if (digits == size_r)
 		return (0);
-	*(r + nums) = '\0';
+	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
 }
