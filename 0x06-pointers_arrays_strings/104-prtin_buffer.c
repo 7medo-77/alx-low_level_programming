63050775398c2e93c863083f91403654a09db9b3
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-23 22:59:52 +0300

    0-memset

--- 0x06-pointers_arrays_strings/104-prtin_buffer.c
+++ 0x06-pointers_arrays_strings/104-prtin_buffer.c
@@ -11,23 +11,22 @@
 
 void print_line(char *c, int s, int l)
 {
-	int j, k;
-	for (j = 0; j <= 9; j++)
-
+int j, k;
+for (j = 0; j <= 9; j++)
 {
-	if (j <= s)
-	printf("%02x", c[l * 10 + j]);
-	else
-	printf("  ");
-	if (j % 2)
-	putchar(' ');
+if (j <= s)
+printf("%02x", c[l * 10 + j]);
+else
+printf("  ");
+if (j % 2)
+putchar(' ');
 }
-	for (k = 0; k <= s; k++)
+for (k = 0; k <= s; k++)
 {
-	if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
 putchar(c[l * 10 + k]);
-	else
-	putchar('.');
+else
+putchar('.');
 }
 }
 
@@ -40,20 +39,20 @@ putchar(c[l * 10 + k]);
  */
 void print_buffer(char *b, int size)
 {
-	int i;
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
 {
-	printf("%08x: ", i * 10);
-	if (i < size / 10)
+printf("%08x: ", i * 10);
+if (i < size / 10)
 {
-	print_line(b, 9, i);
+print_line(b, 9, i);
 }
 else
 {
-	print_line(b, size % 10 - 1, i);
+print_line(b, size % 10 - 1, i);
 }
-	putchar('\n');
+putchar('\n');
 }
-		if (size == 0)
-	putchar('\n');
+if (size == 0)
+putchar('\n');
 }
