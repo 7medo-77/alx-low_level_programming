c4f4d72cea85976b57ea792ea6542f10b577096d
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-25 20:07:49 +0300

    4-pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,12 +2,17 @@
 #include <stdio.h>
 /**
  * _pow_recursion - prints a string, in reverse, followed by a new line
- * @x: number raised to power of y
+ * @n: number raised to power of y
  * @y: nth power of x
+ * Return: -1 in case of input of y which is less than 0 (failure)
+ *			1 in case y is equal to one (success)
  */
-int _pow_recursion(int x, int y)
+int _pow_recursion(int n, int y)
 {
 	if (y < 0)
 		return (-1);
 	else if (y == 0)
+		return (1);
+	n = n * _pow_recursion(n, y - 1);
+	return (n);
 }
