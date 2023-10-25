8e693158ce3398c2e767edcd59ff2f3002f05deb
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-26 01:33:08 +0300

    6-is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+int prime_test(int n, int factor);
+
+/**
+ * prime_test_number - Write a function that returns 1
+ * if the input integer is a prime number, otherwise return 0.
+ *
+ * Return: Always 0 (Success)
+ *
+ * @number: The number to be checked
+ */
+
+int prime_test_number(int number)
+{
+	if (number <= 1)
+	{
+		return (0);
+	}
+
+	return (prime_test(number, 2));
+}
+
+/**
+ * prime_test - Helper function to check for prime numbers recursively.
+ * @n: The integer to check.
+ * @factor: The current factor to test.
+ * Return: 1 if n is prime, 0 otherwise.
+ */
+int prime_test(int n, int factor)
+{
+	if (factor == n)
+	{
+		return (1);
+	}
+	if (n % factor == 0)
+	{
+		return (0);
+	}
+	return (prime_test(n, factor + 1));
+}
