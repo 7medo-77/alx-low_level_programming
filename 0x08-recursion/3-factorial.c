9f5354aabd8621e6917fd2d9c5c62ed8734e0090
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-25 19:09:43 +0300

    3-factorial_3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,9 +12,7 @@ int factorial(int n)
 		return (-1);
 	else if (n == 0)
 		return (1);
-	else
-	{
-		n = n * factorial(n - 1);
-		return (n);
-	}
+
+	n = n * factorial(n - 1);
+	return (n);
 }
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _pow_recursion - prints a string, in reverse, followed by a new line
+ * @x: number raised to power of y
+ * @y: nth power of x
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+}
