5ced3561ae0cb5edc651c01c5f166098505246ab
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-27 16:17:33 +0200

    0-whatismyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point for the program
+ * @argc - Number of arguments passed into function
+ * @argv - Array of command line arguments passed into function
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	printf("%s", argv[0]);
+	return (0);
+}
