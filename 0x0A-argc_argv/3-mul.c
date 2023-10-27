f786088137086e52062eb06fa9cb6874d0c6ad99
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-27 17:20:45 +0200

    3-mul_1

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point for the program
+ * @argc:  Number of arguments passed into function
+ * @argv: Array of command line arguments passed into function
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char const **argv)
+{
+	int res, a, b;
+
+	(void)argc;
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (-1);
+	}
+	else
+	{
+		a = *argv[1] + '0';
+		b = *argv[2] + '0';
+
+		res = a * b;
+		printf("%d\n", res);
+		return (0);
+	}
+}
