beacda24059665704f88a2d3354e855138231121
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-27 16:56:28 +0200

    1-args_1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point for the program
+ * @argc:  Number of arguments passed into function
+ * @argv: Array of command line arguments passed into function
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char const **argv)
+{
+	printf("%d", argc);
+}
