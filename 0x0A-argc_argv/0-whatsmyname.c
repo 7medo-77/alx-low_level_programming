c1b5166323242989ea426dc8a22f0143fa2b80fe
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-27 16:53:54 +0200

    0-whatismyname_2 added proper description for function parameters

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,8 +3,8 @@
 
 /**
  * main - Entry point for the program
- * @argc - Number of arguments passed into function
- * @*argv - Array of command line arguments passed into function
+ * @argc:  Number of arguments passed into function
+ * @argv: Array of command line arguments passed into function
  *
  * Return: 0 (success)
  */
