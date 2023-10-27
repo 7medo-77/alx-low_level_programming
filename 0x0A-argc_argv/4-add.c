e07e4b933816b0ba5fdbf51f65a02c79bc05d305
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-10-27 22:04:04 +0200

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point for the program
+ * @argc:  Number of arguments passed into function
+ * @argv: Array of command line arguments passed into function
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char const **argv)
+{
+	int res, i, j;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (-1);
+	}
+	res = 0;
+	i = 1;
+	j = 0;
+	while (i < argc)
+	{
+		while (argv[i][j] != '\0')
+		{
+			if ((argv[i][j] < '0' || argv[i][j] > '9') && argv[i][j] != '-')
+			{
+				printf("%s\n", "Error");
+				return (-1);
+			}
+			j++;
+		}
+		res += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", res);
+	return (0);
+}
+
