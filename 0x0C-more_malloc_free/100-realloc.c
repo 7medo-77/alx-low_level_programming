c63eff9aa8a7336cd2bb49bcbfe76e60f51e28db
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-03 22:21:38 +0200

    100-realloc_3

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,6 +25,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+	if (ptr == NULL)
+		ptr = malloc(new_size);
 
 	new_ptr = malloc(new_size);
 	if (new_ptr != NULL)
