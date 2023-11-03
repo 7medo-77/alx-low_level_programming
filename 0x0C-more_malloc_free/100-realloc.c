709ac5faaa417ab8a9a39f83ddc4df361a4031c5
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-03 21:50:13 +0200

    100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _realloc - Write a function that allocates
+ * memory for an array, using malloc.
+ *
+ * Return: Always 0 (Success)
+ *
+ * @ptr: the number of elements you want to allocate memory for
+ * @old_size: the size of each element in bytes
+ * @new_size: the
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	void *new_ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	else if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+	if (new_ptr != NULL)
+	{
+		free(ptr);
+		ptr = new_ptr;
+		return (ptr);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
