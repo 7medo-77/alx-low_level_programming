ec3caa4c283524fe10fdae01c07cf1286c32cfac
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-03 03:04:44 +0200

    1-str_nconcat.c_1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+int str_len(char *string);
+
+/**
+ * string_nconcat - allocates memory using malloc to a concatenated string from s1 and s2
+ *
+ * @n: number of bytes to concate from s2 with s1
+ * @s1: String to concate s2 with
+ * @s2: String from which n bytes are concatenated onto s1
+ *
+ * Return: a pointer to the allocated memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int len_s1, len_s2, i, cat_length;
+	char *cat;
+
+	len_s1 = str_len(s1);
+	len_s2 = str_len(s2);
+
+	n >= len_s2 ? n = len_s2 : n;
+	cat_length = len_s1 + n;
+
+	cat = malloc(cat_length * sizeof(char) + 1);
+	i = 0;
+	while (i < cat_length)
+	{
+		if (i < len_s1)
+		{
+			cat[i] = s1[i];
+			cat[i] = s2[i- len_s1];
+		}
+	}
+	cat[i] = '\0';
+	return (cat);
+}
+
+/**
+ * str_len - allocates memory using malloc
+ * @string: String to be counted
+ *
+ * Return: Length of string
+ */
+int str_len(char *string)
+{
+	int i;
+
+	i = 0;
+	while (string[i])
+		i++;
+	return (i);
+}
