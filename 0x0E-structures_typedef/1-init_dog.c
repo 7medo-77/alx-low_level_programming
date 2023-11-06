f0af795fa6dbbb0feac3d72baf26f30856b58d67
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-06 19:42:04 +0200

    dog_h_1: added description

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,9 @@
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+}
+
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,27 @@
 #define DOG_H
 #include <string.h>
 
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Header file containing struct dog and other functions
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
