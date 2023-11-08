920fd00cbfa250dd61183ddf724ee9b396bb9f17
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-08 23:24:13 +0200

    3-main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,16 +18,16 @@ int main(int argc, char **argv)
 	int num1, num2, res = 0;
 	int (*funcP)(int, int);
 
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	funcP = get_op_func(argv[2]);
+	res = funcP(num1, num2);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-	funcP = get_op_func(argv[2]);
-	res = funcP(num1, num2);
 
 	if ((argv[1] == 0 || argv[3] == 0) && argv[2][0] == '/')
 	{
