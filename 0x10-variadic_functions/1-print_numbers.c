d18c8363200290211b308c1da00defa1666c2752
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-10 00:54:04 +0200

    1-print_numbers_1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,1 +1,29 @@
 #include "variadic_functions.h"
+/**
+ * print_numbers - A functoin that prints all the numbers passed to it
+ *
+ * @n: number of arguments
+ * @separator: Delimiter between characters
+ *
+ * Return: Sum of all arguments
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+
+	va_list ap;
+	va_start(ap, n);
+
+	i = 0;
+	while (i < n)
+	{
+		if (i == n - 1)
+		{
+			printf("%d%s\n", va_arg(ap, int), *separator ? *separator : "");
+		}
+		else
+			printf("%d%s", va_arg(ap, int), separator ? separator : "");
+	}
+	va_end(ap);
+}
