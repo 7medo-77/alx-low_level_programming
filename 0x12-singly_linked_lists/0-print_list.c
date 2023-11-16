6f9eb5ea38e952b472bb159f9688f5ff021ca657
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-16 22:01:01 +0200

    0-print_list_2

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		i++;
-		printf("[%d] %s\n", _strlen(h->str), h->str);
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
 	}
 	return (i);
