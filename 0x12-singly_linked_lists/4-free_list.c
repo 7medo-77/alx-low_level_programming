50a8b5bcea67c04ac8db087f7208a1ff02892dd8
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-17 04:22:34 +0200

    4-free_list_betty

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,7 +1,12 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdarg.h>
-/***/
+/**
+ * free_list - Frees dynamically allocated memory reserved
+ *			   for linked list and all its internal elements
+ *
+ * @head: Pointer to the first node of linked list
+ */
 void free_list(list_t *head)
 {
 	list_t *temp;
