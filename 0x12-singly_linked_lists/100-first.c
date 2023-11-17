66c6ba9c7a86a179fdf2d67fb6fa42cfce2162da
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-17 05:04:30 +0200

    100-first_2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,10 +4,12 @@ void _constructor(void) __attribute__ ((constructor));
 /**
  * Print_Before_The_Main_Function - Function that prints a
  * sentence before entry point of the program (main)
+ *
+ * Return: void
  */
 
 void Print_Before_The_Main_Function(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
