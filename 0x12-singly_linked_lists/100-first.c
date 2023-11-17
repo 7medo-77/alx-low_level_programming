b73cc4ff8dff5ec7ca5678627b37e7190260881e
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-17 05:01:22 +0200

    100-first_

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * Print_Before_The_Main_Function - Function that prints a
+ * sentence before entry point of the program (main)
+ */
+
+void Print_Before_The_Main_Function(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
