df203cd94d8154edbfe8bbe7245f1e9549126c4c
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-21 20:41:56 +0200

    6-pop_listint.c_1

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * pop_listint - A function that frees a linked list
+ *
+ * @head: Address of pointer to linked list struct
+ *
+ * Return: Integer content of the head
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int content;
+
+	temp = *head;
+	content = temp->n;
+	*head = (*head)->next;
+
+	return (content);
+}
