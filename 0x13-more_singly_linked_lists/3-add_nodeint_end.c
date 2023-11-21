b771c0dcfabbd0bfbd8bdc77fefb786a637ab55d
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-21 06:29:21 +0200

    3-add_nodeint_end.c

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - A function that prints the number
+ *					 of nodes in a linked list
+ *
+ * @n: Content of the node
+ * @head: pointer to pointer to linked list struct
+ *
+ * Return: new node of struct type listint_t
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *probe, *new;
+
+	probe = *head;
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	while (probe->next)
+	{
+		probe = probe->next;
+	}
+	if (!head)
+		*head = new;
+	else
+		probe->next = new;
+
+	new->next = NULL;
+	return (new);
+}
