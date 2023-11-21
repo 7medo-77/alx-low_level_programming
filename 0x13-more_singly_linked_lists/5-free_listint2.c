d12b441b5cd1e9ca83a1b010f6240e39e0d83cc4
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-21 07:10:47 +0200

    5-free_listint2_2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,6 +11,7 @@ void free_listint2(listint_t **head)
 	if (*head)
 	{
 		probe = *head;
+		*head = NULL;
 		while (probe != NULL)
 		{
 			temp = probe;
@@ -18,9 +19,4 @@ void free_listint2(listint_t **head)
 			free(temp);
 		}
 	}
-	else
-	{
-		*head = NULL;
-	}
-	*head = NULL;
 }
