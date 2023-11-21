4c4c7dc804f6a1b033bd89f9a430e6a3d92e7cf5
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-21 07:03:55 +0200

    5-free_listint2_0

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - A function that frees a linked list
+ *
+ * @head: pointer to pointer to linked list struct
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *probe;
+
+	probe = *head;
+	while (probe != NULL)
+	{
+		temp = probe;
+		probe = probe->next;
+		free(temp);
+	}
+	*head = NULL;
+}
