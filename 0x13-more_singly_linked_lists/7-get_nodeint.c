259bcaabdfe628e2bf8f6a033b3cf2dd05efcec6
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-21 22:01:50 +0200

    7-get_nodeint.c_0

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - A function that frees a linked list
+ *
+ * @head: Address of pointer to linked list struct
+ * @index: Index of the node in the linked list
+ *
+ * Return: pointer to struct in linked list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		i++;
+		head = head->next;
+	}
+	return (NULL);
+}
