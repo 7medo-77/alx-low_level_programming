8cf33d85e925399f6cbc50367ddc30d22da42eb4
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-21 06:17:13 +0200

    2-add_nodeint_1

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * add_nodeint - A function that prints the number of nodes in a linked list
+ *
+ * @n: Content of the node
+ * @head: pointer to pointer to linked list struct
+ *
+ * Return: Number of nodes in a list
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
