2620d616e3aed13adcc748c7a424626c67d89ed6
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-22 04:31:30 +0200

    9-insert_nodeint.c_12

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,8 +36,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			probe->next = new;
 			return (new);
 		}
-		probe = probe->next;
 		i++;
+		probe = probe->next;
 	}
 	if (probe->next == NULL && i == idx)
 	{
