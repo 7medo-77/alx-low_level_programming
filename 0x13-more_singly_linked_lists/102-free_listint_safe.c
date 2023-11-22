1e3895266e37fa2c6da2f919cb59d83b537c5801
7medo-77 <ahmedabdelhamid1997@gmail.com>
2023-11-22 05:11:26 +0200

    102-free_listint_safe.c_2

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,40 +1,46 @@
-#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * free_listint_safe - Safely frees a linked list.
- * @start: Pointer to the first node in the linked list.
+ * free_listint_safe - Frees a linked list the safe way and returns length
  *
- * Return: Number of elements in the freed list.
+ * @header_node: Double pointer to the head of the list
+ *
+ * Return: the size of the list that was free'd.
  */
-size_t free_listint_safe(listint_t **start)
-{
-	size_t length = 0;
-	int difference;
-	listint_t *temporary;
 
-	if (!start || !*start)
+
+size_t free_listint_safe(listint_t **header_node)
+{
+	size_t free_counter = 0;
+	int difference_add;
+	listint_t *curr_node;
+
+	if (!header_node || !*header_node)
 		return (0);
 
-	while (*start)
+	while (*header_node)
 	{
-		difference = *start - (*start)->next;
-		if (difference > 0)
+		difference_add = *header_node - (*header_node)->next;
+
+		if (difference_add > 0)
 		{
-			temporary = (*start)->next;
-			*start = temporary;
-			length++;
+			curr_node = (*header_node)->next;
+			free(*header_node);
+			*header_node = curr_node;
+			free_counter++;
 		}
 		else
 		{
-			*start = NULL;
-			length++;
+			free(*header_node);
+			*header_node = NULL;
+			free_counter++;
 			break;
 		}
 	}
 
-	*start = NULL;
-
-	return (length);
+	*header_node = NULL;
+	return (free_counter);
 }
-
