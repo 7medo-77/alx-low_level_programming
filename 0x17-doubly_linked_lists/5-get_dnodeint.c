6afe9ec8d8799881df1485ca0644845f8edfbab6
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-13 04:22:22 +0200

    5-get_dnodeint.c_1

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
-
+/**
+ * get_dnodeint_at_index - Returns node at given index
+ *
+ * @head: pointer to first node in doubly linked list
+ * @index: Index to search the node
+ *
+ * Return: Pointer to the node at index "index"
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *probe = head;
