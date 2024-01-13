7c470bce697d790069e12b6957a03df5e6a7e159
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-13 04:33:18 +0200

    6-sum_dlistint.c_0

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_dlistint - Returns sum of all values in linked list
+ *
+ * @head: pointer to first node in doubly linked list
+ *
+ * Return: Sum of all the values of nodes in a linked list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *probe = head;
+	int rolling_sum = 0;
+
+	while (probe)
+	{
+		rolling_sum += probe->n;
+	}
+	return (rolling_sum);
+}
