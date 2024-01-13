2bf113a995d7e7462ecb6febf04e927e3b4bd957
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-13 05:07:14 +0200

    7-insert_dnodeint.c_0

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,28 +18,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!new)
 		return (NULL);
 	new->n = n;
-	if (!*h && idx == 0)
+
+	while (probe)
 	{
-		new->next = NULL;
-		new->prev = NULL;
-		*h = new;
-		return (new);
-	}
-	else
-	{
-		while (probe)
+		if (index_test == idx)
 		{
-			if (index_test == idx)
+			if (probe->next == NULL)
+				add_dnodeint_end(h, n);
+			else if (idx == 0)
+				add_dnodeint(h, n);
+			else
 			{
 				new->next = probe;
 				new->prev = probe->prev;
 				probe->prev = new;
 				(probe->prev)->next = new;
-				return (new);
 			}
-			probe = probe->next;
-			index_test++;
+			return (new);
 		}
+		probe = probe->next;
+		index_test++;
 	}
 	return (NULL);
 }
