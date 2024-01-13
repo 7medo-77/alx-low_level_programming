3ffb0d743ceae00f0c792b3b27d6d4250d1a79a4
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-01-13 04:25:04 +0200

    5-get_dnodeint.c_1

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -19,11 +19,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	{
 		if (test_index == index)
 			return (probe);
-		else
-		{
-			probe = probe->next;
-			test_index++;
-		}
+		probe = probe->next;
+		test_index++;
 	}
 	return (NULL);
 }
