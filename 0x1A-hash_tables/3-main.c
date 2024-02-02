b8eb9cb293717ddd9b26d23e864a8041a847911e
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-02-02 23:57:59 +0200

    3-hash_table_set_6

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -12,12 +12,13 @@ int main(void)
 	unsigned long int index;
 
 	ht = hash_table_create(1024);
-	hash_table_set(ht, "lmao", "cool");
-	hash_table_set(ht, "", "empty_key");
-	hash_table_set(ht, "empty_value", "");
+	hash_table_set(ht, "empty_value", "somehting");
 	hash_table_set(ht, "stylist", "haircut");
-	hash_table_set(ht, "beta", "juice");
+	hash_table_set(ht, "joyful", "person");
+	hash_table_set(ht, "one_key", "one_value");
+	hash_table_set(ht, "synaphea", "person_another");
 	hash_table_set(ht, "subgenera", "vice");
+	hash_table_set(ht, "subgenera", "vice_2");
 
 	for (index = 0; index < ht->size; index++)
 	{
