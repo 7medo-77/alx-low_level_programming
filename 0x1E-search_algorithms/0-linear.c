f5fe0bdf70e0905183fec31c9582fcae2c115975
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-05 17:06:32 +0300

    0-linear_17:06

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include <search.h>
+#include <stdio.h>
 /**
  * linear_search - A function that returns the key index from a key string
  *
@@ -10,10 +11,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]", i, array[i])
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (array[i]);
 	}
+	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
 int linear_search(int *array, size_t size, int value);
