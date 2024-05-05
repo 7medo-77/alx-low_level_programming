6851f9737112e5859be3a8e71c52bf97cef77570
7medo-77 <ahmedabdelhamid1997@gmail.com>
2024-05-05 16:59:25 +0300

    0x1E-search_algorithms_16:59

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,19 @@
+#include <search.h>
+/**
+ * linear_search - A function that returns the key index from a key string
+ *
+ * @array: Array to search in
+ * @size: Size of the array
+ * @value: Value to look for in the array
+ *
+ * Return: Length of string
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]", i, array[i])
+	}
+}
